2c977ea32493d6b8a301c35da169197c2d3f366e
tavo15g <tavo15g@gmail.com>
2021-10-03 14:21:16 -0500

    modifi app-ui

--- kanban-ui/src/environments/environment.prod.ts
+++ kanban-ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  kanbanAppUrl: '/api'
+  kanbanAppUrl: '${API_URL_PROD}'
 };
--- kanban-ui/src/environments/environment.ts
+++ kanban-ui/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  kanbanAppUrl: 'http://localhost:8080'
+  kanbanAppUrl: '${API_URL_DEV}'
 };
 
 /*
